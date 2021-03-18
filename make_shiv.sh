2ed96559c9bad0d609c9d86dd4a360f17ac90f60
Jon Hurst <jon.a@hursts.org.uk>
2021-03-18 15:27:19 +0000

    Use a more robust #!

--- make_shiv.sh
+++ make_shiv.sh
@@ -4,6 +4,6 @@ PROJ=/home/jon/proj/aimslite
 virtualenv venv
 source venv/bin/activate
 pip install $PROJ
-shiv -c aimsgui -o aimsgui.pyw $PROJ
+shiv -c aimsgui -o aimsgui.pyw -p "/usr/bin/env python3" $PROJ
 deactivate
 rm -r venv
